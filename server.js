ca4d9f99613b3708d24538861cbea08bd1a945ae
Wayne254-art <waynegiyabe6@gmail.com>
2025-05-12 21:23:17 +0300

    corrected controllers

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from  'cors'
+import cors from 'cors'
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,12 +13,12 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-app.use(cors({ 
+app.use(cors({
 	origin: "http://localhost:3000",
 	// origin: "https://chatme-26916.web.app", 
 	methods: "GET,POST,PUT,DELETE",
-	credentials: true            
-  }));
+	credentials: true
+}));
 
 const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
@@ -31,11 +31,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 server.listen(PORT, () => {
 	connectToMongoDB();
